42e31f9156906bee84230c5b62ec0d539178e05f
NAFIZ009 <jalalahmednafiz@gmail.com>
2022-10-25 19:48:04 +0600

    all data passing

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ const express=require('express');
 const app = express();
 const port =5000;
 const data=require('./courseData/course.json');
+app.get('/courses', (req, res) => {
+    res.send(data);
+});
