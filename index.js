b9071ac02256222199ce3cf8a3e89ec1a7da4510
NAFIZ009 <jalalahmednafiz@gmail.com>
2022-10-25 19:49:01 +0600

    dynamic url

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ const data=require('./courseData/course.json');
 app.get('/courses', (req, res) => {
     res.send(data);
 });
+app.get('/courses/:id', (req, res) => {
+    const id=req.params.id;
+    console.log(data);
+    const filtredData=data.find(data=>data.id==id);
+    res.send(filtredData);
+});
