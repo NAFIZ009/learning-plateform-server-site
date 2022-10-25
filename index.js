feff18c880a51fe0e299a6039a98416b962a7e99
NAFIZ009 <jalalahmednafiz@gmail.com>
2022-10-25 20:52:50 +0600

    cors

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express=require('express');
 const app = express();
 const port =5000;
 const data=require('./courseData/course.json');
+const cors=require('cors');
+app.use(cors());
 app.get('/courses', (req, res) => {
     res.send(data);
 });
